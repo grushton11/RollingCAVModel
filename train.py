7192a9a17c31f69f7b89c37cd1d9ce8188a039f2
GeorgeRushton <50720829+GeorgeRushton@users.noreply.github.com>
2020-01-27 15:36:12 +0000

    Added required imports

--- train.py
+++ train.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+import pandas as pd
+import numpy as np
+import math
+import os
+from datetime import datetime as dt
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
+from collections import Counter
+from sklearn.metrics import (accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix, roc_curve, auc)
+from sklearn.metrics import (r2_score, explained_variance_score, mean_absolute_error, mean_squared_error, median_absolute_error)
+from sklearn.model_selection import train_test_split
+import math
+import matplotlib.pyplot as plt
+from dateutil import parser
+from dateutil.relativedelta import relativedelta
+import joblib
+import xgboost as xgb
+from datetime import datetime as dt
+
 def _payment_type_mapping(var_value):
     if var_value in ['CreditCard', 'CreditCardReferenceTransaction']:
         return 'Credit Card'
