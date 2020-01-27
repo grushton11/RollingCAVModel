76b8fd656eba112f2b87cbc5c9fc89e67bce0cf3
GeorgeRushton <50720829+GeorgeRushton@users.noreply.github.com>
2020-01-27 15:02:30 +0000

    Added several functions to train

--- train.py
+++ train.py
@@ -11,14 +11,16 @@ def append_dummies_list(categoricals, shortlists):
                 dummy_list.append('{}_{}'.format(i,j))
     return dummy_list
 
+
 def preprocessing_function(df,
                            current_tm_list,
                            target_month_list,
                            preferred_sports_shortlist,
                            preferred_competition_shortlist,
-                           acquisiton_cohort_shortlist, 
+                           acquisiton_cohort_shortlist,
                            preferred_competition_dist_shortlist,
-                           preferred_sport_dist_shortlist):
+                           preferred_sport_dist_shortlist,
+                           payment_method_shortlist):
 
     # Unify payment types (e.g. reference CreditCard and Credit Card both as Credit Card)
     payment_method_feature_list = CAVmodel.prepare_feature_payment_type(df)
@@ -135,3 +137,122 @@ def preprocessing_function(df,
     df_features = df.loc[:,required_train_set_columns].copy()
 
     return df_features, model_diagnostics, class_labels
+
+def get_x_and_y_train(current_tm_list,
+                      target_month_list,
+                      df_features_train,
+                      synchronization_time_days,
+                      model_diagnostics):
+
+    # Create empty dict for X and Y train sets per model
+    X_train_dict = {}
+    Y_train_dict = {}
+
+    # Bring in the training calculation dates from the DSS project variables
+    train_date = dataiku.get_custom_variables()['training_calculation_date']
+    training_start_date = dataiku.get_custom_variables()['training_start_date']
+
+    # For each current tenure month
+    for i, current_tm in enumerate(current_tm_list):
+
+        # For each target month per current tenure month
+        for k, target_month in enumerate(target_month_list):
+            # As long as the target month is greater or equal to the current month
+            if k >= i:
+
+                target_month_num = 2 + k
+                print('building training set for current tm {} and target month {}'.format(current_tm, target_month))
+                current_tm_df = df_features_train[df_features_train['tenure_month'] == current_tm - 1]
+
+                # create end and start period for training
+                training_end_date = parser.parse(train_date) + relativedelta(months=-target_month_num, days = - synchronization_time_days)
+
+                # convert datetime to string
+                training_start_date_str = training_start_date
+                training_end_date_str = training_end_date.strftime('%Y-%m-%d')
+
+                # Create mask filters based on the calculation date and apsply using the access start date field
+                start_mask = current_tm_df['access_start_date'] >= training_start_date_str
+                end_mask = current_tm_df['access_start_date'] <= training_end_date_str
+                print("current tenure month: {}, train tenure month: {}, binary target tenure month: {}".format(current_tm, current_tm-1, target_month))
+                current_tm_df = current_tm_df.loc[start_mask & end_mask]
+
+                # Define the class labels to drop
+                print("initial train sample size     : " + str(current_tm_df.shape[0]))
+                class_labels_to_drop = list(target_month_list)
+                class_labels_to_drop.remove(target_month)
+                print("class_labels_to_drop: {}".format(class_labels_to_drop))
+
+                # Downsample to have a balanced split of the target
+                class_weight = current_tm_df[target_month].sum() / 1.0 / current_tm_df.shape[0]
+                print("class weight: " + str(class_weight))
+                sample_size = current_tm_df[target_month].sum()
+                df_features_train_sampled = current_tm_df.groupby(target_month, group_keys=False).apply(lambda group: group.sample(sample_size, replace=True))
+
+                # Output to console
+                total_train = df_features_train_sampled.shape[0]
+                percentage_target_train = df_features_train_sampled[target_month].sum() / float(total_train)
+                print("downsampled train sample size     : " + str(total_train))
+                print("% target class        : " + str(percentage_target_train))
+                print('tm_{}_target_{}'.format(current_tm,target_month) + ': train_samples: ' + str(df_features_train_sampled.shape[0]) + '; start_time:', df_features_train_sampled['access_start_date'].min().strftime('%Y-%m-%d'), '; end_time:', df_features_train_sampled['access_start_date'].max().strftime('%Y-%m-%d'))
+
+                # Output to the model diagnostics dict
+                model_diagnostics['tm_{}_target_{}'.format(current_tm,target_month)]['training_samples_available'] = df_features_train_sampled.shape[0]
+                model_diagnostics['tm_{}_target_{}'.format(current_tm,target_month)]['train_start_date'] = df_features_train_sampled['access_start_date'].min().strftime('%Y-%m-%d')
+                model_diagnostics['tm_{}_target_{}'.format(current_tm,target_month)]['train_end_date'] = df_features_train_sampled['access_start_date'].max().strftime('%Y-%m-%d')
+
+                # Create train X and Y
+                X_train = df_features_train_sampled.copy()
+                X_train = X_train.drop(class_labels_to_drop, axis=1)
+                X_train = X_train.drop(target_month, axis=1)
+                X_train = X_train.drop(["access_start_date", "tenure_length_capped", "is_churn"], axis=1)
+                Y_train = df_features_train_sampled.loc[:,[target_month]].values.ravel()
+
+                # Add the X and Y train sets to the dict for all models
+                X_train_dict['tm_{}_target_{}'.format(current_tm,target_month)] = X_train
+                Y_train_dict['tm_{}_target_{}'.format(current_tm,target_month)]  = Y_train
+
+    return X_train_dict, Y_train_dict, model_diagnostics
+
+
+def train_models(current_tm_list, target_month_list, X_train_dict, Y_train_dict, model_diagnostics, rfc):
+
+    model_list = []
+    rfc_models = {}
+
+    for i, current_tm in enumerate(current_tm_list):
+
+        for k, target_month in enumerate(target_month_list):
+            if k >= i:
+
+                # train model
+                start = dt.now()
+                print("training tm {} target {} model".format(current_tm,target_month))
+
+                rfc_model = rfc.fit(X_train_dict['tm_{}_target_{}'.format(current_tm, target_month)], Y_train_dict['tm_{}_target_{}'.format(current_tm, target_month)])
+
+                print("training tm {} target {} model took: {}".format(current_tm, target_month, dt.now()-start))
+                print("trained tm {} target {} model with oob score: ".format(current_tm, target_month) + str(rfc_model.oob_score_))
+
+                model_list.append(rfc_model)
+                rfc_models['tm_{}_target_{}'.format(current_tm, target_month)] = rfc_model
+
+                # Output to console
+                total_train = Y_train_dict['tm_{}_target_{}'.format(current_tm, target_month)].shape[0]
+                total_target_class = Y_train_dict['tm_{}_target_{}'.format(current_tm, target_month)].sum()
+                class_weight = total_target_class / float(total_train)
+
+                model_diagnostics['tm_{}_target_{}'.format(current_tm, target_month)]['oob_score'] = rfc_model.oob_score_
+                model_diagnostics['tm_{}_target_{}'.format(current_tm, target_month)]['model_parameter'] = rfc
+
+                model_diagnostics['tm_{}_target_{}'.format(current_tm, target_month)]['total_train_samples_used'] = total_train
+                model_diagnostics['tm_{}_target_{}'.format(current_tm, target_month)]['total_train_samples_target_class'] = total_target_class
+                model_diagnostics['tm_{}_target_{}'.format(current_tm, target_month)]['total_train_percentage_target_class'] = class_weight
+                model_diagnostics['tm_{}_target_{}'.format(current_tm, target_month)]['sampling_strategy'] = 'No sampling strategy'
+
+                train_preds = rfc_model.predict(X_train_dict['tm_{}_target_{}'.format(current_tm, target_month)])
+                model_diagnostics['tm_{}_target_{}'.format(current_tm, target_month)]['train_set_accuracy_score'] = accuracy_score(Y_train_dict['tm_{}_target_{}'.format(current_tm, target_month)], train_preds)
+                model_diagnostics['tm_{}_target_{}'.format(current_tm, target_month)]['train_set_precision_score'] = precision_score(Y_train_dict['tm_{}_target_{}'.format(current_tm, target_month)], train_preds)
+                model_diagnostics['tm_{}_target_{}'.format(current_tm, target_month)]['train_set_recall_score'] = recall_score(Y_train_dict['tm_{}_target_{}'.format(current_tm, target_month)], train_preds)
+
+    return rfc_models
