ea03a96e0cce405c0076d45072800435a9262173
GeorgeRushton <50720829+GeorgeRushton@users.noreply.github.com>
2020-01-27 11:41:50 +0000

    Create train.py

---
+++ train.py
@@ -0,0 +1,4 @@
+def cyclical_feature_encode(data, col, max_val):
+    data[col + '_sin'] = np.sin(2 * np.pi * data[col]/max_val)
+    data[col + '_cos'] = np.cos(2 * np.pi * data[col]/max_val)
+    return data
